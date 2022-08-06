6bec41d0851b1f7c0bbc0f06d3dc125a965dbe1e
hakimhammadache <h.hakim@yalidine.com>
2022-08-06 15:03:27 +0200

    first commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,69 @@
+import { DataApiService } from './../../services/http/data-api.service';
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  skills;
+  featuredProjects;
+  workExperience;
+  profile;
+
+  showMobileImages = false;
+
+  constructor(
+    public dataApi: DataApiService
+  ) { }
+
+  @HostListener('window:resize', ['$event'])
+  onresize(event): void {
+    console.log('WINDOW_RESIZE_EVENT', event);
+    this.checkWindowSize();
+  }
+
+  private checkWindowSize(): void {
+    window.innerWidth <= 768
+      ? this.showMobileImages = true
+      : this.showMobileImages = false;
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.checkWindowSize();
+
+
+    this.skills = await this.dataApi.getTopSkills();
+    console.log('SKILLS', this.skills);
+
+    this.featuredProjects = await this.dataApi.getFeaturedProjects();
+    console.log('PROJECTS', this.featuredProjects);
+
+    this.workExperience = await this.dataApi.getWorkHistory();
+    console.log('WORK', this.workExperience);
+
+
+    this.profile = await this.dataApi.getProfile();
+    console.log('PROFILE', this.profile);
+
+  }
+
+  async downloadResume(): Promise<void> {
+    const pdf: any = await this.dataApi.getBase64CV().subscribe({
+      next: (response) => {
+        this.openFile(response, 'application/pdf');
+      },
+    });;
+  }
+  openFile(data: any, type: string) {
+    let blob = new Blob([data], { type: type });
+    let url = window.URL.createObjectURL(blob);
+    let pwa = window.open(url);
+    if (!pwa || pwa.closed || typeof pwa.closed == 'undefined') {
+      alert('Please disable your pop-up blocker and try again!');
+    }
+  }
+
+}
